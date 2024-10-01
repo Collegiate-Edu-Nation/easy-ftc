17027d3980fddedc9c604395b8726041b9d9b40a
camdenboren <boren.camden@gmail.com>
2024-10-01 10:48:59 -0500

    updated servo util tests

--- easy-ftc/src/test/java/org/cen/easy_ftc/TestServoMecanismUtil.java
+++ easy-ftc/src/test/java/org/cen/easy_ftc/TestServoMecanismUtil.java
@@ -1,25 +1,25 @@
-package org.cen.easy_ftc.claw;
+package org.cen.easy_ftc;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-public class TestClawUtil {
+public class TestServoMecanismUtil {
     @Test
     public void controlToDirection_isCorrect() {
         // Test open
-        double result = ClawUtil.controlToDirection(1, 0, 0, true, false);
+        double result = ServoMecanismUtil.controlToDirection(1, 0, 0, true, false);
         assertEquals(1, result, 0.01);
 
         // Test close
-        result = ClawUtil.controlToDirection(1, 0, 1, false, true);
+        result = ServoMecanismUtil.controlToDirection(1, 0, 1, false, true);
         assertEquals(0, result, 0.01);
 
         // Test doNothing (both false)
-        result = ClawUtil.controlToDirection(1, 0, 1, false, false);
+        result = ServoMecanismUtil.controlToDirection(1, 0, 1, false, false);
         assertEquals(1, result, 0.01);
 
         // Test doNothing (both true)
-        result = ClawUtil.controlToDirection(1, 0, 1, true, true);
+        result = ServoMecanismUtil.controlToDirection(1, 0, 1, true, true);
         assertEquals(1, result, 0.01);
     }
 }
