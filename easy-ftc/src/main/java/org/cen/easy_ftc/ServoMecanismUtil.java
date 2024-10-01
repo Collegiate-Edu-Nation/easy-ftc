e0fb2aaff1926346f89fdbebb531672c58ca5fdf
camdenboren <boren.camden@gmail.com>
2024-10-01 10:48:32 -0500

    moved clawutil to top level servomecanismutil for later generatlizability

---
+++ easy-ftc/src/main/java/org/cen/easy_ftc/ServoMecanismUtil.java
@@ -0,0 +1,28 @@
+package org.cen.easy_ftc;
+
+/**
+ * Blueprints an abstract servo utility class, providing a method for controlling directions, which
+ * is common to all servo mecanisms. Cannot be instantiated, only extended by other classes
+ * providing static methods.
+ * <p>
+ * 
+ * @Methods {@link #controlToDirection(double open, double close, double current, boolean openButton, boolean closeButton)}
+ *          (used by subclasses)
+ */
+public abstract class ServoMecanismUtil {
+    /**
+     * Set claw servo movement based on open, close values as well as current position
+     */
+    public static double controlToDirection(double open, double close, double current,
+            boolean openButton, boolean closeButton) {
+        double movement;
+        if (openButton && !closeButton) {
+            movement = open;
+        } else if (closeButton && !openButton) {
+            movement = close;
+        } else { // do nothing otherwise
+            movement = current;
+        }
+        return movement;
+    }
+}
