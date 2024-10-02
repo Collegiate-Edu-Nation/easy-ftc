145885648f03d0c86a8fe56bd68db516a98c9958
camdenboren <boren.camden@gmail.com>
2024-10-02 14:57:22 -0500

    finish replacing cen

--- easy-ftc/src/androidTest/java/org/edu_nation/easy_ftc/ExampleInstrumentedTest.java
+++ easy-ftc/src/androidTest/java/org/edu_nation/easy_ftc/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package org.cen.easy_ftc;
+package org.edu_nation.easy_ftc;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("org.cen.easy_ftc", appContext.getPackageName());
+        assertEquals("org.edu_nation.easy_ftc", appContext.getPackageName());
     }
 }
