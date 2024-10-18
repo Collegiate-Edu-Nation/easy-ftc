c6008c26d7ce513c7daab42d208c3298311d10f7
camdenboren <boren.camden@gmail.com>
2024-10-18 13:21:14 -0500

    consolidate drive

--- easy-ftc/src/main/java/org/edu_nation/easy_ftc/drive/Drive.java
+++ easy-ftc/src/main/java/org/edu_nation/easy_ftc/drive/Drive.java
@@ -5,17 +5,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 /**
- * Blueprints an abstract drivetrain, providing basic functionalities, options, and objects common
- * to all drivetrains. Cannot be instantiated, only extended by actual drivetrain classes (see
- * {@link Mecanum} and {@link Differential}).
+ * Implements a drivetrain by extending the functionality of {@link MotorMechanism}.
  * <p>
  * 
- * @Methods {@link #wait(double time)} (inherited from {@link Mechanism})
+ * @param LinearOpMode opMode (required)
+ * @param HardwareMap hardwareMap (required)
+ * @param Boolean useEncoder (true or false)
+ * @param Double diameter (> 0.0)
+ * @param Gamepad gamepad (gamepad1 or gamepad2)
+ * @param String layout ("tank" or "arcade")
+ *        <p>
+ * @Methods {@link #tele()}
+ *          <li>{@link #move(double power, String direction, double measurement)}
+ *          <li>{@link #reverse()}
+ *          <li>{@link #reverse(String motorName)}
+ *          <li>{@link #setAllPower(double [] movements)}
+ *          <li>{@link #setAllPower()} (defaults to array of zeros if nothing is passed)
+ *          <li>{@link #wait(double time)} (inherited from {@link Drive})
  */
-abstract class Drive extends MotorMechanism {
+public class Drive extends MotorMechanism {
+    private IMU imu;
+    private String type;
+
     /**
      * Constructor
      * 
@@ -24,8 +42,8 @@ abstract class Drive extends MotorMechanism {
      *           <li>gamepad = null
      *           <li>layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap) {
-        this(opMode, hardwareMap, false);
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type) {
+        this(opMode, hardwareMap, type, false);
     }
 
     /**
@@ -35,8 +53,8 @@ abstract class Drive extends MotorMechanism {
      *           <li>gamepad = null
      *           <li>layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder) {
-        this(opMode, hardwareMap, useEncoder, "");
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder) {
+        this(opMode, hardwareMap, type, useEncoder, "");
     }
 
     /**
@@ -46,8 +64,8 @@ abstract class Drive extends MotorMechanism {
      *           <li>diameter = 0.0
      *           <li>layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, Gamepad gamepad) {
-        this(opMode, hardwareMap, false, gamepad);
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, Gamepad gamepad) {
+        this(opMode, hardwareMap, type, false, gamepad);
     }
 
     /**
@@ -57,8 +75,8 @@ abstract class Drive extends MotorMechanism {
      *           <li>diameter = 0.0
      *           <li>gamepad = null
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String layout) {
-        this(opMode, hardwareMap, false, layout);
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, String layout) {
+        this(opMode, hardwareMap, type, false, layout);
     }
 
     /**
@@ -66,9 +84,9 @@ abstract class Drive extends MotorMechanism {
      * 
      * @Defaults layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder,
             double diameter) {
-        this(opMode, hardwareMap, useEncoder, diameter, "");
+        this(opMode, hardwareMap, type, useEncoder, diameter, "");
     }
 
     /**
@@ -77,9 +95,9 @@ abstract class Drive extends MotorMechanism {
      * @Defaults diameter = 0.0
      *           <li>layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder,
             Gamepad gamepad) {
-        this(opMode, hardwareMap, useEncoder, gamepad, "");
+        this(opMode, hardwareMap, type, useEncoder, gamepad, "");
     }
 
     /**
@@ -88,8 +106,8 @@ abstract class Drive extends MotorMechanism {
      * @Defaults diameter = 0.0
      *           <li>gamepad = null
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder, String layout) {
-        this(opMode, hardwareMap, useEncoder, null, layout);
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder, String layout) {
+        this(opMode, hardwareMap, type, useEncoder, null, layout);
     }
 
     /**
@@ -98,8 +116,8 @@ abstract class Drive extends MotorMechanism {
      * @Defaults useEncoder = false
      *           <li>diameter = 0.0
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, Gamepad gamepad, String layout) {
-        this(opMode, hardwareMap, false, gamepad, layout);
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, Gamepad gamepad, String layout) {
+        this(opMode, hardwareMap, type, false, gamepad, layout);
     }
 
     /**
@@ -107,9 +125,9 @@ abstract class Drive extends MotorMechanism {
      * 
      * @Defaults diameter = 0.0
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder, Gamepad gamepad,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder, Gamepad gamepad,
             String layout) {
-        this(opMode, hardwareMap, useEncoder, 0.0, gamepad, layout);
+        this(opMode, hardwareMap, type, useEncoder, 0.0, gamepad, layout);
     }
 
     /**
@@ -117,9 +135,9 @@ abstract class Drive extends MotorMechanism {
      * 
      * @Defaults gamepad = null
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder, double diameter,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder, double diameter,
             String layout) {
-        this(opMode, hardwareMap, useEncoder, diameter, null, layout);
+        this(opMode, hardwareMap, type, useEncoder, diameter, null, layout);
     }
 
     /**
@@ -127,18 +145,24 @@ abstract class Drive extends MotorMechanism {
      * 
      * @Defaults layout = ""
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder, double diameter,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder, double diameter,
             Gamepad gamepad) {
-        this(opMode, hardwareMap, useEncoder, diameter, gamepad, "");
+        this(opMode, hardwareMap, type, useEncoder, diameter, gamepad, "");
     }
 
     /**
      * Constructor
      */
-    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, boolean useEncoder, double diameter,
+    public Drive(LinearOpMode opMode, HardwareMap hardwareMap, String type, boolean useEncoder, double diameter,
             Gamepad gamepad, String layout) {
         this.opMode = opMode;
         this.hardwareMap = hardwareMap;
+        this.type = type;
+        if (type == "differential") {
+            this.numMotors = 2;
+        } else {
+            this.numMotors = 4;
+        }
         this.useEncoder = useEncoder;
         this.diameter = diameter;
         this.gamepad = gamepad;
@@ -147,5 +171,192 @@ abstract class Drive extends MotorMechanism {
         hardwareInit();
     }
 
-    public abstract void move(double power, String direction, double measurement);
+    /**
+     * Initializes drive motors based on constructor args (e.g. using encoders or not)
+     */
+    @Override
+    protected void hardwareInit() {
+        if (useEncoder) {
+            // Instantiate motors
+            motorsEx = new DcMotorEx[numMotors];
+
+            if (numMotors == 4) {
+                motorsEx[0] = hardwareMap.get(DcMotorEx.class, "frontLeft");
+                motorsEx[1] = hardwareMap.get(DcMotorEx.class, "frontRight");
+                motorsEx[2] = hardwareMap.get(DcMotorEx.class, "backLeft");
+                motorsEx[3] = hardwareMap.get(DcMotorEx.class, "backRight");
+            } else {
+                motorsEx[0] = hardwareMap.get(DcMotorEx.class, "driveLeft");
+                motorsEx[1] = hardwareMap.get(DcMotorEx.class, "driveRight");
+            }
+
+            MotorConfigurationType[] motorTypes = getMotorTypes();
+
+            // Reverse direction of left motors for convenience (switch if robot drives backwards)
+            setDirections();
+
+            // Reset encoders
+            setModesEx(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+            // Set motors to run using the encoder (velocity, not position)
+            setModesEx(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+            if (diameter == 0.0) {
+                velocityMultiplier = getAchieveableMaxTicksPerSecond(motorTypes);
+            } else {
+                distanceMultiplier = getTicksPerRev(motorTypes);
+            }
+        } else {
+            // Instantiate motors
+            motors = new DcMotor[numMotors];
+            if (numMotors == 4) {
+                motors[0] = hardwareMap.get(DcMotor.class, "frontLeft");
+                motors[1] = hardwareMap.get(DcMotor.class, "frontRight");
+                motors[2] = hardwareMap.get(DcMotor.class, "backLeft");
+                motors[3] = hardwareMap.get(DcMotor.class, "backRight");
+            } else {
+                motors[0] = hardwareMap.get(DcMotor.class, "driveLeft");
+                motors[1] = hardwareMap.get(DcMotor.class, "driveRight");
+            }
+
+            // Reverse direction of left motors for convenience (switch if robot drives backwards)
+            setDirections();
+
+            // Set motors to run without the encoders (power, not velocity or position)
+            setModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        // Initializes imu for field-centric layout. Adjust "UP" and "FORWARD" if orientation is
+        // reversed
+        if (layout == "field") {
+            imu = hardwareMap.get(IMU.class, "imu");
+            IMU.Parameters parameters = new IMU.Parameters(
+                    new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+            imu.initialize(parameters);
+            imu.resetYaw();
+        }
+    }
+
+    /**
+     * Enables teleoperated mecanum movement with gamepad (inherits layout).
+     * <p>
+     * Calling this directly is one of the primary use-cases of this class.
+     * <p>
+     * Basic mecanum algorithm largely taken from the BasicOmniOpMode block example.
+     * <p>
+     * Field-centric algorithm taken from:
+     * https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+     */
+    @Override
+    public void tele() {
+        double heading = 0;
+        // Press option to reset imu to combat drift, set heading if applicable
+        if (layout == "field") {
+            heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            if (gamepad.options) {
+                imu.resetYaw();
+            }
+        }
+        
+        double[] movements = DriveUtil.controlToDirection(type, layout, deadZone, heading,
+                gamepad.left_stick_y, gamepad.left_stick_x, gamepad.right_stick_y, gamepad.right_stick_x);
+        setAllPower(movements);
+    }
+
+    /**
+     * Intermediate function that assigns individual motor powers based on direction specified in
+     * runOpMode() calls.
+     * <p>
+     * Calling this directly is one of the primary use-cases of this class.
+     * <p>
+     * Valid directions are: forward, backward, left, right, rotateLeft, rotateRight, forwaredLeft,
+     * forwardRight, backwardLeft, backwardRight
+     */
+    public void move(double power, String direction, double measurement) {
+        double[] movements = DriveUtil.languageToDirection(type, power, direction);
+
+        if (diameter == 0.0) {
+            setAllPower(movements);
+            wait(measurement);
+            setAllPower();
+        } else {
+            double[] unscaledMovements = DriveUtil.languageToDirection(type, 1, direction);
+            int[] positions = DriveUtil.calculatePositions(measurement, diameter,
+                    distanceMultiplier, unscaledMovements);
+            int[] currentPositions = getCurrentPositions();
+
+            // move the motors at power until they've reached the position
+            setPositions(positions, currentPositions);
+            setAllPower(movements);
+            while (motorsAreBusy()) {
+                setAllPower(movements);
+            }
+            setAllPower();
+
+            // Reset motors to run using velocity (allows for using move() w/ diameter along w/
+            // tele())
+            setModesEx(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    /**
+     * Reverse the direction of the specified motor
+     */
+    public void reverse(String motorName) {
+        if (numMotors == 4) {
+            switch (motorName) {
+                case "frontLeft":
+                    if (useEncoder) {
+                        motorsEx[0].setDirection(DcMotorEx.Direction.FORWARD);
+                    } else {
+                        motors[0].setDirection(DcMotor.Direction.FORWARD);
+                    }
+                    break;
+                case "frontRight":
+                    if (useEncoder) {
+                        motorsEx[1].setDirection(DcMotorEx.Direction.REVERSE);
+                    } else {
+                        motors[1].setDirection(DcMotor.Direction.REVERSE);
+                    }
+                    break;
+                case "backLeft":
+                    if (useEncoder) {
+                        motorsEx[2].setDirection(DcMotorEx.Direction.FORWARD);
+                    } else {
+                        motors[2].setDirection(DcMotor.Direction.FORWARD);
+                    }
+                    break;
+                case "backRight":
+                    if (useEncoder) {
+                        motorsEx[3].setDirection(DcMotorEx.Direction.REVERSE);
+                    } else {
+                        motors[3].setDirection(DcMotor.Direction.REVERSE);
+                    }
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected motorName: " + motorName
+                            + ", passed to Lift.reverse(). Valid names are: frontLeft, frontRight, backLeft, backRight");
+            }
+        } else {
+            switch (motorName) {
+                case "driveLeft":
+                    if (useEncoder) {
+                        motorsEx[0].setDirection(DcMotorEx.Direction.FORWARD);
+                    } else {
+                        motors[0].setDirection(DcMotor.Direction.FORWARD);
+                    }
+                    break;
+                case "driveRight":
+                    if (useEncoder) {
+                        motorsEx[1].setDirection(DcMotorEx.Direction.REVERSE);
+                    } else {
+                        motors[1].setDirection(DcMotor.Direction.REVERSE);
+                    }
+                    break;
+                default:
+                    throw new IllegalArgumentException("Unexpected motorName: " + motorName
+                            + ", passed to Lift.reverse(). Valid names are: driveLeft, driveRight");
+            }
+        }
+    }
 }
---
+++ easy-ftc/src/test/java/org/edu_nation/easy_ftc/drive/TestDrive.java
@@ -0,0 +1,239 @@
+package org.edu_nation.easy_ftc.drive;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+public class TestDrive {
+    LinearOpMode mockedOpMode = mock(LinearOpMode.class);
+    HardwareMap mockedHardwareMap = mock(HardwareMap.class);
+    Gamepad mockedGamepad = mock(Gamepad.class);
+    DcMotor mockedMotor = mock(DcMotor.class);
+    DcMotorEx mockedMotorEx = mock(DcMotorEx.class);
+    MotorConfigurationType motorType = new MotorConfigurationType();
+
+    private void mockInit() {
+        when(mockedHardwareMap.get(DcMotor.class, "driveLeft")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotor.class, "driveRight")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotorEx.class, "driveLeft")).thenReturn(mockedMotorEx);
+        when(mockedHardwareMap.get(DcMotorEx.class, "driveRight")).thenReturn(mockedMotorEx);
+        when(mockedHardwareMap.get(DcMotor.class, "frontLeft")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotor.class, "frontRight")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotor.class, "backLeft")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotor.class, "backRight")).thenReturn(mockedMotor);
+        when(mockedHardwareMap.get(DcMotorEx.class, "frontLeft")).thenReturn(mockedMotorEx);
+        when(mockedHardwareMap.get(DcMotorEx.class, "frontRight")).thenReturn(mockedMotorEx);
+        when(mockedHardwareMap.get(DcMotorEx.class, "backLeft")).thenReturn(mockedMotorEx);
+        when(mockedHardwareMap.get(DcMotorEx.class, "backRight")).thenReturn(mockedMotorEx);
+        when(mockedMotorEx.getMotorType()).thenReturn(motorType);
+        when(mockedMotorEx.isBusy()).thenReturn(true, false);
+    }
+
+    @Test
+    public void Drive_initializes() {
+        mockInit();
+
+        try {
+            new Drive(mockedOpMode, mockedHardwareMap, "differential");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true);
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", "");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", "arcade");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4);
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", mockedGamepad, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4, mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, mockedGamepad, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4, mockedGamepad, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true);
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", "");
+            // new Drive(mockedOpMode, mockedHardwareMap, "mecanum", "field");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4);
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", mockedGamepad, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4, mockedGamepad);
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, mockedGamepad, "");
+            new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4, mockedGamepad, "");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void teleMec_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", mockedGamepad);
+            Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, mockedGamepad);
+            drive.tele();
+            driveEnc.tele();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void teleDif_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential", mockedGamepad);
+            Drive driveEnc =
+                    new Drive(mockedOpMode, mockedHardwareMap, "differential", true, mockedGamepad);
+            drive.tele();
+            driveEnc.tele();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void moveDif_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential");
+            Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "differential", true);
+            Drive drivePos = new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4);
+            drive.move(0.5, "forward", 1);
+            driveEnc.move(0.5, "forward", 1);
+            drivePos.move(0.5, "forward", 12);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void moveMec_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum");
+            Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true);
+            Drive drivePos = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4);
+            drive.move(0.5, "forward", 1);
+            driveEnc.move(0.5, "forward", 1);
+            drivePos.move(0.5, "forward", 12);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void reverseDif_isCalledWithValidInputs() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential");
+            Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "differential", true);
+            drive.reverse();
+            drive.reverse("driveLeft");
+            drive.reverse("driveRight");
+            driveEnc.reverse();
+            driveEnc.reverse("driveLeft");
+            driveEnc.reverse("driveRight");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void reverseMec_isCalledWithValidInputs() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum");
+            Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true);
+            drive.reverse();
+            drive.reverse("frontLeft");
+            drive.reverse("frontRight");
+            drive.reverse("backLeft");
+            drive.reverse("backRight");
+            driveEnc.reverse();
+            driveEnc.reverse("frontLeft");
+            driveEnc.reverse("frontRight");
+            driveEnc.reverse("backLeft");
+            driveEnc.reverse("backRight");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void reverseDif_ThrowsException() {
+        mockInit();
+
+        Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential", mockedGamepad);
+        Drive driveEnc =
+                new Drive(mockedOpMode, mockedHardwareMap, "differential", true, mockedGamepad);
+        drive.reverse("abc");
+        drive.reverse("");
+        driveEnc.reverse("abc");
+        driveEnc.reverse("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void reverseMec_ThrowsException() {
+        mockInit();
+
+        Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", mockedGamepad);
+        Drive driveEnc = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, mockedGamepad);
+        drive.reverse("abc");
+        drive.reverse("");
+        driveEnc.reverse("abc");
+        driveEnc.reverse("");
+    }
+
+    @Test
+    public void setGearingDif_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4);
+            drive.setGearing(19.2);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void setGearingMec_isCalled() {
+        mockInit();
+
+        try {
+            Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4);
+            drive.setGearing(19.2);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setGearingDif_ThrowsException() {
+        mockInit();
+
+        Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "differential", true, 4);
+        drive.setGearing(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setGearingMec_ThrowsException() {
+        mockInit();
+
+        Drive drive = new Drive(mockedOpMode, mockedHardwareMap, "mecanum", true, 4);
+        drive.setGearing(0);
+    }
+}
