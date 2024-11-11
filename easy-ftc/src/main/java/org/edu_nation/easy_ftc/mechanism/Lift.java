52a59f1a36c210229f61b0f7ca190fbfa5e67768
camdenboren <boren.camden@gmail.com>
2024-11-11 16:47:00 -0600

    rm unused import

--- easy-ftc/src/main/java/org/edu_nation/easy_ftc/mechanism/Lift.java
+++ easy-ftc/src/main/java/org/edu_nation/easy_ftc/mechanism/Lift.java
@@ -5,7 +5,6 @@
 package org.edu_nation.easy_ftc.mechanism;
 
 import java.util.Arrays;
-import java.lang.Math;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotor;
